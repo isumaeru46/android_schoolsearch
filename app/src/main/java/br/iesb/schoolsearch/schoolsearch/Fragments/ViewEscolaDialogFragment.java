aac43519b75c602d7f844af39750309491cff78f
Ismael <you@example.com>
2017-11-28 14:51:26 -0200

    impl

--- app/src/main/java/br/iesb/schoolsearch/schoolsearch/Fragments/ViewEscolaDialogFragment.java
+++ app/src/main/java/br/iesb/schoolsearch/schoolsearch/Fragments/ViewEscolaDialogFragment.java
@@ -8,12 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import br.iesb.schoolsearch.schoolsearch.R;
 import br.iesb.schoolsearch.schoolsearch.models.EscolaModel;
 
 public class ViewEscolaDialogFragment extends DialogFragment {
+
+    private Button teste;
+    private EscolaModel escola;
 
     public ViewEscolaDialogFragment() {
     }
@@ -21,7 +26,8 @@ public class ViewEscolaDialogFragment extends DialogFragment {
     public static ViewEscolaDialogFragment newInstance(EscolaModel escola) {
         ViewEscolaDialogFragment frag = new ViewEscolaDialogFragment();
         Bundle args = new Bundle();
-        args.putString("title", escola.getNome());
+        args.putSerializable("escola",escola);
+        //args.putString("title", escola.getNome());
         frag.setArguments(args);
         return frag;
     }
@@ -29,6 +35,16 @@ public class ViewEscolaDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_view_escola, container);
+
+        teste = (Button) v.findViewById(R.id.buttonMaps);
+
+        teste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(),"teste",Toast.LENGTH_SHORT);
+            }
+        });
+
         return v;
     }
 
@@ -41,6 +57,7 @@ public class ViewEscolaDialogFragment extends DialogFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        escola = (EscolaModel) getArguments().getSerializable("escola");
         setStyle(DialogFragment.STYLE_NORMAL, getTheme());
     }
 
@@ -55,13 +72,16 @@ public class ViewEscolaDialogFragment extends DialogFragment {
         // Get field from view
         EditText mEditText;
         mEditText = (EditText) view.findViewById(R.id.txt_your_name);
+        mEditText.setText(escola.getNome());
         // Fetch arguments from bundle and set title
         String title = getArguments().getString("title", "Enter Name");
         getDialog().setTitle(title);
         // Show soft keyboard automatically and request focus to field
         mEditText.requestFocus();
-        getDialog().getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+        //getDialog().getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
         getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
+
+
 
 }
